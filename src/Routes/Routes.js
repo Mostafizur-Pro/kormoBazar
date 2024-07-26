ca0333999ec2df8844175bfbd34a937768459151
Mostafizur-Pro <mostafizur0195@gmail.com>
2024-07-26 11:00:25 +0600

    create route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,12 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import HomePage from "../Pages/Home/HomePage";
+import Login from "../Pages/Auth/Login/Login";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
-
-
-  export default router;
+export default router;
